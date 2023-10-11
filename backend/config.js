87629e41326d4c41c5a68de95878013b42eb6239
Sahil Gulati <sahilgulati49@gmail.com>
2023-10-11 09:23:45 -0400

    url updated

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export const PORT = 5555;
  
-// export const mongoDBURL = 'mongodb+srv://sgmongodb:sgweb@cluster0.szfnwwr.mongodb.net/books-collection?retryWrites=true&w=majority';
+
 
 export const mongoDBURL = 'mongodb://127.0.0.1:27017/bookcollection';
